739073e8727c833363686ac351312b4f6e1326a6
Ozzy <Ozols.ugiss@gmail.com>
2022-03-09 13:51:45 +0200

    dagger hilt test setup

--- app/src/androidTest/java/com/ugisozols/shoppinglistapp/ShoppingListAppE2E.kt
+++ app/src/androidTest/java/com/ugisozols/shoppinglistapp/ShoppingListAppE2E.kt
@@ -1,6 +1,19 @@
 package com.ugisozols.shoppinglistapp
 
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+
+@HiltAndroidTest
 class ShoppingListAppE2E {
 
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun setUp(){
+        hiltRule.inject()
+    }
 
 }
