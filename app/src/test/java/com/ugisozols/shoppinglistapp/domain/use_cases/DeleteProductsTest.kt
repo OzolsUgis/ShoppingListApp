49bbbb1ae830f192e9cdc1d733ee80d65cbece92
Ozzy <Ozols.ugiss@gmail.com>
2022-03-09 12:20:49 +0200

    delete functionality testing

--- app/src/test/java/com/ugisozols/shoppinglistapp/domain/use_cases/DeleteProductsTest.kt
+++ app/src/test/java/com/ugisozols/shoppinglistapp/domain/use_cases/DeleteProductsTest.kt
@@ -1,7 +1,13 @@
 package com.ugisozols.shoppinglistapp.domain.use_cases
 
+import com.google.common.truth.Truth.assertThat
+import com.ugisozols.shoppinglistapp.R
 import com.ugisozols.shoppinglistapp.data.repository.FakeShoppingListRepository
-import org.junit.Assert.*
+import com.ugisozols.shoppinglistapp.domain.models.Category
+import com.ugisozols.shoppinglistapp.domain.models.Product
+import com.ugisozols.shoppinglistapp.utils.Resource
+import com.ugisozols.shoppinglistapp.utils.UiText
+import kotlinx.coroutines.runBlocking
 
 import org.junit.Before
 import org.junit.Test
@@ -17,5 +23,29 @@ class DeleteProductsTest {
     }
 
     @Test
-    fun `Empty list `
+    fun `Empty list, should return error`() : Unit = runBlocking{
+        val list = emptyList<Product>()
+        val testResponse = deleteProducts(list)
+
+        assertThat(
+            testResponse == Resource.Error(UiText.StringResource(R.string.delete_list_empty), null)
+        )
+    }
+
+    @Test
+    fun `List contains objects, should return success`() : Unit = runBlocking {
+        val list = mutableListOf(
+            Product(
+                name = "Pipe",
+                amount = "7mm",
+                category = Category.House,
+                checked = true
+            )
+        )
+        val testResponse = deleteProducts(list)
+
+        assertThat(
+            testResponse == Resource.Success(null)
+        )
+    }
 }
